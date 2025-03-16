2a0dfec9417c9c8aa6f03687dc9ac4d48d4860bb
Adrian-Augusto <adriansilva7272@gmail.com>
2025-03-16 15:05:57 -0300

    Update Cliente.js

--- Cliente.js
+++ Cliente.js
@@ -1,4 +1,8 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Cliente = void 0;
+var Cliente = /** @class */ (function () {
+    // Construtor
     function Cliente(nome) {
         this.nome = nome;
     }
